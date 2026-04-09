a09e716d0c958bc5ac90bc1f92b0b1ffe85efd7f
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-09 14:06:49 -0300

    2757.C

---
+++ 2757.C
@@ -0,0 +1,42 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+int a, b, c;
+
+
+a<=10000 && a>=-10000;
+b<=99 && b>=0;
+c<=999 && c>=0;
+
+printf("A = %d", a);
+
+printf("B = %d", b);
+
+printf("C = %d", c);
+
+
+printf("A =10%d", a);
+
+printf("B =10%d", b);
+
+printf("C =10%d", c);
+
+
+
+printf("A =%010d", a);
+
+printf("B =010%d", b);
+
+printf("C =%010d", c);
+
+
+printf("A =%-10d", a);
+
+printf("B =%-10d", b);
+
+printf("C =%-10d", c);
+
+
+    return 0;
+}
