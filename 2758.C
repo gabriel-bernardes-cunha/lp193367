15d1b136a21f3d9a67060ce1e3a758dab3b55c0b
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-09 15:11:54 -0300

    2758.C

--- 2758.C
+++ 2758.C
@@ -1,1 +1,44 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+float a, b;
+double c, d;
 
+scanf("%f",&a);
+scanf("%f",&b);
+scanf("%lf",&c);
+scanf("%lf",&d);
+
+
+printf("A = %f, ",&a);
+printf("B = %f\n",&b);
+printf("C = %lf, ",&c);
+printf("D = %lf\n",&d);
+
+printf("A = %0.1f, ",&a);
+printf("B = %0.1f\n",&b);
+printf("C = %0.1lf, ",&c);
+printf("D = %0.1lf\n",&d);
+
+printf("A = %0.2f, ",&a);
+printf("B = %0.2f\n",&b);
+printf("C = %0.2lf, ",&c);
+printf("D = %0.2lf\n",&d);
+
+printf("A = %0.3f, ",&a);
+printf("B = %0.3f\n",&b);
+printf("C = %0.3lf, ",&c);
+printf("D = %0.3lf\n",&d);
+
+printf("A = %0.3Ef, ",&a);
+printf("B = %0.3Ef\n",&b);
+printf("C = %0.3Elf, ",&c);
+printf("D = %0.3Elf\n",&d);
+
+printf("A = %0.0f, ",&a);
+printf("B = %0.0f\n",&b);
+printf("C = %0.0lf, ",&c);
+printf("D = %0.0lf\n",&d);
+    return 0;
+}
