2c702f2a84ce9640d99052b1811d91c7860ad77a
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-14 17:06:15 -0300

    Atualizar o 2757.C

--- 2757.C
+++ 2757.C
@@ -2,43 +2,43 @@
 
 int main() {
  
-int a, b, c;
+    int a, b, c;
 
-scanf("%d",&a);
+    scanf("%d",&a);
 
-scanf("%d",&b);
+    scanf("%d",&b);
 
-scanf("%d",&c);
-
-
-
-printf("A = %d, ", a);
-
-printf("B = %d, ", b);
-
-printf("C = %d\n", c);
+    scanf("%d",&c);
 
 
-printf("A = %10d, ", a);
 
-printf("B = %10d, ", b);
+    printf("A = %d, ", a);
 
-printf("C = %10d\n", c);
+    printf("B = %d, ", b);
 
-
-printf("A = %010d, ", a);
-
-printf("B = %010d, ", b);
-
-printf("C = %010d\n", c);
+    printf("C = %d\n", c);
 
 
-printf("A = %-10d, ", a);
+    printf("A = %10d, ", a);
 
-printf("B = %-10d, ", b);
+    printf("B = %10d, ", b);
 
-printf("C = %-10d\n", c);
+    printf("C = %10d\n", c);
 
 
-    return 0;
+    printf("A = %010d, ", a);
+
+    printf("B = %010d, ", b);
+
+    printf("C = %010d\n", c);
+
+
+    printf("A = %-10d, ", a);
+
+    printf("B = %-10d, ", b);
+
+    printf("C = %-10d\n", c);
+
+
+return 0;
 }
