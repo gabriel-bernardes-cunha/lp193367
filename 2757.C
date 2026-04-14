9c4d6dac10eff2cd1684d29c43a07427f3a3c86a
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-14 18:38:51 -0300

    Atualizar o 2757.C

--- 2757.C
+++ 2757.C
@@ -5,38 +5,25 @@ int main() {
     int a, b, c;
 
     scanf("%d",&a);
-
     scanf("%d",&b);
-
     scanf("%d",&c);
 
-
-
     printf("A = %d, ", a);
-
     printf("B = %d, ", b);
-
     printf("C = %d\n", c);
 
 
     printf("A = %10d, ", a);
-
     printf("B = %10d, ", b);
-
     printf("C = %10d\n", c);
 
-
     printf("A = %010d, ", a);
-
     printf("B = %010d, ", b);
-
     printf("C = %010d\n", c);
 
 
     printf("A = %-10d, ", a);
-
     printf("B = %-10d, ", b);
-
     printf("C = %-10d\n", c);
 
 
