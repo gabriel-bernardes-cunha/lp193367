997b9bf5db922e04fedbd3f9d8fbe31998fd2eae
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-14 23:00:38 -0300

    Update 1071.c

--- 1071.c
+++ 1071.c
@@ -4,14 +4,15 @@ Linguagem   : C
 Plataforma  : https://judge.beecrowd.com/pt/search
 Exercício   : 1071
 Data        : 15/04/2026
-Objetivo    : L
+Objetivo    : Escrever a soma dos valores ímpares que estão entre os valores 
+fornecidos na entrada que deverá caber em um inteiro.
 Aprendizado : 
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 int main() {
     int x,y;
-    
+    scanf("%d%d",&x&y);
     
  
    
