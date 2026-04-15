29b04d8a3782798b2207ee64e5c75209652fd614
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-14 22:17:26 -0300

    Create 1071.c

---
+++ 1071.c
@@ -0,0 +1,19 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1071
+Data        : 15/04/2026
+Objetivo    : L
+Aprendizado : 
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+int main() {
+    int x,y;
+    
+    
+ 
+   
+return 0;
+}
