135ea3ce5bcc7fa22e61ec7e393a8b3263f9b64a
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-14 22:32:59 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : NNNN
+Data        : 07/04/2026
+Objetivo    : Escrever a mensagem "Hello World!"
+Aprendizado : Como começ
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main()
