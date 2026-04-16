34e96c3bc1f42d5a0ec814f03e3b8535a2aaaf54
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-16 20:54:11 -0300

    Update 2757.C

--- 2757.C
+++ 2757.C
@@ -1,13 +1,14 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Gabriel Bernardes Cunha
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 2757
+Problema    : https://judge.beecrowd.com/pt/problems/view/2757
 Data        : 09/04/2026
 Objetivo    : Escrever numeros inteiros de variadas formas
 Aprendizado : Dominar printf e a forma que um numero sera escrita, como sua 
 justificativa e notacao
 -------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
