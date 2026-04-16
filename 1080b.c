8cb4c3080537f30104c8f4498a194fbac03a87a0
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-16 20:47:11 -0300

    Create 1080b.c

---
+++ 1080b.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main () {
+    int v[100], i, posicao, maior=0;
+    for (i=0;i<100;i++){
+        scanf("%d",&v[i]);
+        
+        if (v[i]>maior) {
+            maior=v[i];
+            posicao=i;
+        }
+    }
+    printf("%d\n",maior);
+    printf("%d\n",posicao++);
+    return 0;
+}
