41d6adf167d6fd63420b6f26295ff79f00d0f0fb
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-16 20:49:25 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Gabriel Bernardes Cunha
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1000
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
 Data        : 07/04/2026
 Objetivo    : Escrever a mensagem "Hello World!"
 Aprendizado : Como começar e terminar um codigo e usar o comando printf
