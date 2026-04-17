e193dc60552e5f4c932c604fabf76336702733eb
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-16 21:22:21 -0300

    Update 1080a.c

--- 1080a.c
+++ 1080a.c
@@ -1,12 +1,16 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Gabriel Bernardes Cunha
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 17/04/2026
+Objetivo    : Ler 100 numeros, motrar o maior e sua posicao num vetor
+Aprendizado : 
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 int main () {
-    int v[100], i, posicao, maior=0;
-    for (i=0;i<100;i++){
-        scanf("%d",&v[i]);
-        
-        if (v[i]>maior) {
-            maior=v[i];
-            posicao=i;
+    
         }
     }
     printf("%d\n",maior);
