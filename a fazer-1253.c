6bd5854f6057bda06916896234e578e7708c5d12
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-26 11:32:26 -0300

    Update a fazer-1253.c

--- a fazer-1253.c
+++ a fazer-1253.c
@@ -10,27 +10,25 @@ Aprendizado :
 #include <stdio.h>
  
 int main() {
- 
+    char saida[50];
     int N, a=0;
     int b=0;
     scanf("%d",&N);
-    for (int i=0;i<N;i++) {
-        
-        char saida[50];
-        
+    for (int i=0;i<N;i++) {  
+     
         scanf ("%s",saida);
         scanf("%d",&a);
         a=a%26;
         
         while(saida[b]!='\0') {
-            if (saida[b]-a<65 && saida[b]-a>60 || saida[b]-a<97 && saida[b]-a>){
-                saida[b]='Z'
+            if ((saida[b]-a<'a' && saida[b]-a>'A') || (saida[b]-a<'A')){
+                saida[b]+=26;
             }
-            if (
             saida[b]=saida[b]-a;
             b++;
         }
         printf("%s\n",saida);
+        b=0; 
     }
  
     return 0;
