5f023a1a7d355604310a7f1ba5cae661a9020aab
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-26 11:56:31 -0300

    Update and rename a fazer-1253.c to 1253a.c

--- 1253.c
+++ 1253.c
@@ -3,15 +3,15 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Gabriel Bernardes Cunha
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1253
-Data        : 25/04/2026
-Objetivo    : 
-Aprendizado : 
+Data        : 26/04/2026
+Objetivo    : Decodificar uma eventual cifra de cesar
+Aprendizado : posições em strings (usando contadores), valores de strings (com base na ASCII), aguns valores da ASCII
 -------------------------------------------------------------------------- */
 #include <stdio.h>
  
 int main() {
     char saida[50];
-    int N, a=0;
+    int N, a=0; 
     int b=0;
     scanf("%d",&N);
     for (int i=0;i<N;i++) {  
@@ -21,7 +21,8 @@ int main() {
         a=a%26;
         
         while(saida[b]!='\0') {
-            if ((saida[b]-a<'a' && saida[b]-a>'A') || (saida[b]-a<'A')){
+            // se o deslocamento sair do alfabeto, adiciona-se 26
+            if ((saida[b]-a<'a' && saida[b]>'a') || (saida[b]-a<'A')){
                 saida[b]+=26;
             }
             saida[b]=saida[b]-a;
@@ -30,6 +31,5 @@ int main() {
         printf("%s\n",saida);
         b=0; 
     }
- 
     return 0;
 }
