440eabd55fd333d7554cb1862112fbda0e5ba88a
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-26 21:49:06 -0300

    Update a fazer 1234.c

--- a fazer 1234.c
+++ a fazer 1234.c
@@ -5,7 +5,7 @@ Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1234
 Data        : 25/04/2026
 Objetivo    : Transformar uma frase em uma letra maiúscula e em seguida uma minúscula, sucessivamente
-Aprendizado : Ler frases (strings), usar tolower/toupper, usar frase[i]='caractere', usar 
+Aprendizado : Ler frases (strings), usar tolower/toupper, usar frase[i]='caractere', usar fgets stfin, strcspn e o uso do NULL
 -------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <ctype.h> 
@@ -16,23 +16,25 @@ int main() {
     char frase[51];
     int i=0;
     int n=0;
-    
-    fgets(frase,51,stdin);
+    fgets(frase, 51, stdin);
     frase[strcspn(frase,"\n")]='\0';
     
-    while (frase[i]!='\0'){
-       if (frase[i]!=' ') {
-        if (n%2==0) {
-            frase[i]=toupper(frase[i]);
+    // não sei o que errei, conferir depois
+    
+        while (frase[i]!='\0'){
+         if (frase[i]!=' ') {
+            if (n%2==0) {
+                frase[i]=toupper(frase[i]);
         
+         }
+         else    {
+                frase[i]=tolower(frase[i]);
+         }
+         n++;    
+         }
+         i++;
         }
-        else {
-            frase[i]=tolower(frase[i]);
-        }
-        n++;
-       }
-       i++;
-    }
-    printf("%s"frase);
+        printf("%s\n",frase);
+    }    
     return 0;
 }
