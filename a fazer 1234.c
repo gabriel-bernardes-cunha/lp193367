1f4fe000eb689f481c4bbc3bc6e87f2619744a0e
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-04-28 21:55:52 -0300

    Update a fazer 1234.c

--- a fazer 1234.c
+++ a fazer 1234.c
@@ -3,9 +3,9 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Gabriel Bernardes Cunha
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1234
-Data        : 25/04/2026
+Data        : 28/04/2026
 Objetivo    : Transformar uma frase em uma letra maiúscula e em seguida uma minúscula, sucessivamente
-Aprendizado : Ler frases (strings), usar tolower/toupper, usar frase[i]='caractere', usar fgets stfin, strcspn ...
+Aprendizado : Ler frases (strings), usar tolower/toupper, usar frase[i]='caractere', usar fgets stfin, strcspn e NULL
 -------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <ctype.h> 
@@ -14,43 +14,12 @@ Aprendizado : Ler frases (strings), usar tolower/toupper, usar frase[i]='caracte
 int main() {
     
     char frase[51];
-    int i=0;
-    int n=0;
-    fgets(frase, 51, stdin);
-    frase[strcspn(frase,"\n")]='\0';
     
-    while (frase[i]!='\0'){
-        if (frase[i]!=' ') {
-            if (n%2==0) {
-                frase[i]=toupper(frase[i]);
-        
-            }
-            else {
-                frase[i]=tolower(frase[i]);
-            }
-        n++;    
-        }
-    i++;
-    }
-    printf("%s\n",frase);
-    return 0;
-}
-
-______________________________________
-#include <stdio.h>
-#include <ctype.h> 
-#include <string.h>
-
-int main() {
     
-    char frase[51];
-    int i=0;
-    int n=0;
-    
-    frase[strcspn(frase,"\n")]='\0';
-    
-    while (1) {
-        fgets(frase, 51, stdin);
+    while (fgets(frase, 51, stdin) != NULL) {
+        frase[strcspn(frase,"\n")]='\0';
+        int i=0;
+        int n=0;
         
         while (frase[i]!='\0'){
          if (frase[i]!=' ') {
@@ -65,10 +34,8 @@ int main() {
         }
         i++;
         }
-    if (strlen(frase)==0) {
-        break;
-    }
+        
+        printf("%s\n",frase);
     }    
-    printf("%s\n",frase);
     return 0;
 }
